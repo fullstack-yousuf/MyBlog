20126c2992cf6cff420c9e72c290838113cc355a
Muhammad Yousuf <fullstack.yousuf@gmail.com>
2025-10-29 18:55:01 +0500

    update the vercel url in login

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -27,7 +27,7 @@ const LoginPage = () => {
         { name: "email", type: "email", placeholder: "Email" },
         { name: "password", type: "password", placeholder: "Password" },
       ]}
-      endpoint="http://myblogyousuf-backend.vercel.app/auth/login"
+      endpoint="https://myblogyousuf-backend.vercel.app/auth/login"
       submitLabel="Login"
       onSuccess={handleSuccess}
     />
